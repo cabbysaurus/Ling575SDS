ad6aecd1e4bd97da3acdf04e8583dc03f8273311
Nick Chen <nickchen@gmail.com>
2017-05-09 22:36:18 -0700

    - output average sentiment by system

--- analyze.py
+++ analyze.py
@@ -11,10 +11,12 @@ import csv
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from collections import defaultdict
 from difflib import SequenceMatcher
+from nltk import word_tokenize
 
 class Analyze(object):
     def __init__(self):
         self._tasks = {}
+        self.sid = SentimentIntensityAnalyzer()
 
     def add_row(self, row):
         task_id = row[0].rsplit("_", 1)[0]
@@ -24,7 +26,7 @@ class Analyze(object):
 
     def summarize(self):
         print "Total: ", len(self._tasks)
-        heading = ["REPE"]
+        heading = ["REPE", "SIGH", "SENT"]
         print "  SYS  TOTAL",
         for head in heading:
             print "%12s" % (head),
@@ -33,31 +35,41 @@ class Analyze(object):
             total = self._systems[system]
             print "%5s: %5d" % (system, total),
             for att in heading:
-                count = {
-                    "REPE": self._repeated_by_system
-                }.get(att)(system)
-                print "%5d (%.2f)" % (count, float(count)/float(total)),
+                out = {
+                    "REPE": self._repeated_by_system,
+                    "SIGH": self._sigh_by_system,
+                    "SENT": self._sentiment_by_system,
+                }.get(att)(system, total)
+                print "%12s" % (out),
             print ""
 
-    def _repeated_by_system(self, system):
+    def _repeated_by_system(self, system, total):
+        count = self._by_system(system, "_have_repeated")
+        return "%5d (%.2f)" % (count, float(count)/float(total))
+
+    def _sigh_by_system(self, system, total):
+        count = self._by_system(system, "_have_sigh")
+        return "%5d (%.2f)" % (count, float(count)/float(total))
+
+    def _by_system(self, system, key):
         count = 0
         for task in filter(lambda t: t.system == system, self._tasks.itervalues()):
-            if task._have_repeated:
+            if getattr(task, key):
                 count += 1
         return count
 
-    def count_by_system(self):
-        c = defaultdict(int)
-        for task_id in self._tasks.iterkeys():
-            system = task_id.split("_")[-2]
-            c[system] += 1
-        return c
+    def _sentiment_by_system(self, system, count):
+        s = []
+        for task in filter(lambda t: t.system == system, self._tasks.itervalues()):
+            s.append(task._compound)
+        ret = sum(s)/float(len(s))
+        return "%12s" % ("%.2f" % ret)
 
     def analyze(self):
         self._systems = defaultdict(int)
         for task in self._tasks.itervalues():
             self._systems[task.system] += 1
-            task.analyze()
+            task.analyze(self)
 
 class Sentence(object):
     def __init__(self, row):
@@ -65,6 +77,26 @@ class Sentence(object):
         self.system = row[1]
         self.asr = row[2]
         self.transcribed = row[3]
+        self.filtered = []
+        self._compound = 0.0
+        self.transcribed_tokens = []
+        (self.transcribed_tokens, self.filtered) = self.filter_bracket(word_tokenize(self.transcribed))
+        self.transcribed_sanitized = " ".join(self.transcribed_tokens)
+        # print self.id, self.filtered, self.transcribed_sanitized
+
+    def filter_bracket(self, tokens):
+        ret = []
+        filtered = []
+        bracket = 0
+        for t in tokens:
+            if t == ']': bracket -= 1
+            elif t == '[': bracket += 1
+            if bracket > 0 or t == ']':
+                if t not in ["[", "]"]:
+                    filtered.append(t)
+                continue
+            ret.append(t)
+        return (ret, filtered)
 
 class Task(object):
     def __init__(self, task_id):
@@ -73,6 +105,8 @@ class Task(object):
         self.system = task_id.split("_")[-2]
             # attributes
         self._have_repeated = False
+        self._have_sigh = False
+        self._compound = 0.0
 
     def append(self, row):
         self._sentences.append(Sentence(row))
@@ -90,8 +124,25 @@ class Task(object):
                 self._have_repeated = True
                 break
 
-    def analyze(self):
+    def _analyze_sentiment(self, parent):
+        for s in self._sentences:
+            if 'sigh' in s.filtered:
+                self._have_sigh = True
+                break
+        c = []
+        for s in self._sentences:
+            ss = parent.sid.polarity_scores(s.transcribed_sanitized)
+            s._compound = ss.get('compound')
+            if s._compound != 0.0:
+                c.append(s._compound)
+        if len(c) > 0:
+            self._compound = sum(c)/float(len(c))
+        else:
+            self._compound = 0.0
+
+    def analyze(self, parent):
         self._analyze_repeated_questions()
+        self._analyze_sentiment(parent)
 
 def main(argv=sys.argv):
     filepath = "raw.csv"
